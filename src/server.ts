934120bd56c1563e5e50137a8d9508cb4f5a4be7
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2025-07-08 19:09:25 -0300

    route post room: create a new room

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,29 @@
-import { fastifyCors } from "@fastify/cors";
-import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
-} from "fastify-type-provider-zod";
+} from 'fastify-type-provider-zod';
 
-import { env } from "./env.ts";
-import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+import { env } from './env.ts';
+import { createRoomRoute } from './http/routes/create-room.ts';
+import { getRoomsRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-  origin: "http://localhost:5173",
+  origin: 'http://localhost:5173',
 });
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
-app.get("/health", () => {
-  return "OK";
+app.get('/health', () => {
+  return 'OK';
 });
 
 app.register(getRoomsRoute);
+app.register(createRoomRoute);
 
 app.listen({ port: env.PORT });
